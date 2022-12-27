881c8acf4e592fd8a93c233b843e5e6968789ead
m-danya <danila-mikh@ya.ru>
2022-12-27 17:02:43 +0300

    Add the "About" button

--- react-frontend/src/Header.js
+++ react-frontend/src/Header.js
@@ -1,14 +1,30 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import InfoIcon from "@mui/icons-material/Info";
+import Grid from "@mui/material/Grid";
 
 class Header extends React.Component {
   render() {
     return (
       <div>
-        <Typography variant="h3" style={{ margin: "30px 0" }}>
-          Prolog testing system
-        </Typography>
+        <Grid container spacing={2} justifyContent="space-between" alignItems="center">
+          <Grid item>
+            <Typography variant="h3" style={{ margin: "30px 0" }}>
+              Prolog testing system
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Button
+              variant="text"
+              startIcon={<InfoIcon />}
+              href="https://github.com/m-danya/prolog-testing-system"
+              target="_blank"
+            >
+              About
+            </Button>
+          </Grid>
+        </Grid>
       </div>
     );
   }
