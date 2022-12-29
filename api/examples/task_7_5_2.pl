88467f467d82e154abaa2789f8f47383bbd48bd5
m-danya <danila-mikh@ya.ru>
2022-12-29 12:53:30 +0300

    Expand readme + use more markdown in descriptions

--- api/examples/task_7_5_2.pl
+++
@@ -1,5 +0,0 @@
-reach(V, E, X, Y) :- member(X, V), member(Y, V), reach_(E, X, Y).
-
-reach_([[X, Y]|_], X, Y).
-reach_([[X, Z]|T], X, Y) :- reach_(T, Z, Y).
-reach_([[_, _]|T], X, Y) :- reach_(T, X, Y).
