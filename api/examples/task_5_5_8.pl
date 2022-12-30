367594db07c3761a952bd317fcd682a31ffd0079
Notlebedev <artem-itf@yandex.ru>
2022-12-30 16:12:30 +0300

    Update example with correct argument order

--- api/examples/task_5_5_8.pl
+++ api/examples/task_5_5_8.pl
@@ -1,11 +1,16 @@
 my_reverse([], []).
-my_reverse([H|T], R) :-
-    my_reverse_helper(T, R, [H]).
+my_reverse(R, [H|T]) :-
+    my_reverse_loop(R, T, [H]).
 
-my_reverse_helper([], R, R).
-my_reverse_helper([H|T], R, Acc) :-
-    my_reverse_helper(T, R, [H|Acc]).
+my_reverse_loop(R, [], R).
+my_reverse_loop(R, [H|T], Acc) :-
+    my_reverse_loop(R, T, [H|Acc]).
 
 % есть еще более понятная версия с append
 % my_reverse([], []).
-% my_reverse([H|T], R) :- my_reverse(T, RT), append(RT, [H], R).
+% my_reverse(R, [H|T]) :- my_reverse(RT, T), append(RT, [H], R).
+
+my_reverse([], []).
+my_reverse(X, Y) :- rev_int(X, Y, []).
+rev_int(X, [], X).
+rev_int(X, [A|R1], R2) :- rev_int(X, R1, [A|R2]).
