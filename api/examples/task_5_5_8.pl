3c05f17a4b1c731841786546719ee7a1a862a922
Notlebedev <artem-itf@yandex.ru>
2022-12-30 16:17:57 +0300

    Remove accidental copy from example

--- api/examples/task_5_5_8.pl
+++ api/examples/task_5_5_8.pl
@@ -9,8 +9,3 @@ my_reverse_loop(R, [H|T], Acc) :-
 % есть еще более понятная версия с append
 % my_reverse([], []).
 % my_reverse(R, [H|T]) :- my_reverse(RT, T), append(RT, [H], R).
-
-my_reverse([], []).
-my_reverse(X, Y) :- rev_int(X, Y, []).
-rev_int(X, [], X).
-rev_int(X, [A|R1], R2) :- rev_int(X, R1, [A|R2]).
