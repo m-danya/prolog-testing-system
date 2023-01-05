67da2e310536d78d3528780ceb74c64963d06fb5
Notlebedev <artem-itf@yandex.ru>
2023-01-05 19:09:20 +0300

    Extract common code

---
+++ api/tests/task_7_2_2/common.py
@@ -0,0 +1,17 @@
+import re
+
+def test(output_lines, test_number, correct_lines, expected):
+    if any("Fatal Error: TL" in line for line in output_lines):
+        return (test_number, "TL", output_lines, correct_lines)
+    if any("exception" in line for line in output_lines):
+        return (test_number, "RE", output_lines, correct_lines)
+
+    p = re.compile(r'Result = \[\[\[(.*?)\]\]\]')
+    results = p.search(output_lines[0])
+    if results is None or results.group(1) is None:
+        return (test_number, "WA: output mismatch", output_lines, correct_lines)
+    
+    nums = {int(s) for s in results.group(1).split(',')}
+    if nums != expected:
+        return (test_number, "WA: output mismatch", output_lines, correct_lines)
+    return (test_number, "OK", output_lines, correct_lines)
--- api/tests/task_7_2_2/test_2.py
+++ api/tests/task_7_2_2/test_2.py
@@ -1,18 +1,6 @@
+from common import test
 correct_lines = ['Result = [[[1,3,5,17]]]', 'yes']
 expected = set([1, 3, 5, 17])
 
 def test_result(output_lines, test_number):
-    if any("Fatal Error: TL" in line for line in output_lines):
-        return (test_number, "TL", output_lines, correct_lines)
-    if any("exception" in line for line in output_lines):
-        return (test_number, "RE", output_lines, correct_lines)
-
-    p = re.compile(r'Result = \[\[\[(.*?)\]\]\]')
-    results = p.search(output_lines[0])
-    if results is None or results.group(1) is None:
-        return (test_number, "WA: output mismatch", output_lines, correct_lines)
-    
-    nums = {int(s) for s in results.group(1).split(',')}
-    if nums != expected:
-        return (test_number, "WA: output mismatch", output_lines, correct_lines)
-    return (test_number, "OK", output_lines, correct_lines)
+    return test(output_lines, test_number, correct_lines, expected)
